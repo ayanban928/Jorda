8ab9f06af6f41198a378ddb7cee4da28604d6a56
Ayan Banerjee <ayanbanerj@gmail.com>
2026-01-27 01:00:02 -0500

    debugging

--- server/server.ts
+++ server/server.ts
@@ -2,9 +2,9 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
-import authRoutes from './routes/auth.ts';
-import sheetRoutes from './routes/sheets.ts';
-import jobRoutes from './routes/jobs.ts';
+import authRoutes from './routes/auth';
+import sheetRoutes from './routes/sheets';
+import jobRoutes from './routes/jobs';
 
 dotenv.config();
 
